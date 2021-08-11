0540bc733c4f35181a60a346216f057c855f37e4
czl1378 <70316304+easydeal-io@users.noreply.github.com>
2021-08-11 08:59:47 +0800

    init commit

---
+++ netlify/functions/send.js
@@ -0,0 +1,54 @@
+const nodemailer = require("nodemailer");
+
+const { 
+  SMTP_HOST, 
+  SMTP_PORT, 
+  EMAIL_ACCOUNT, 
+  EMAIL_PASSWORD 
+} = process.env;
+
+const sendTo = async (to, title, content) => {
+ 
+  const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT || 587,
+    secure: SMTP_PORT == 465 ? true : false,
+    auth: {
+      user: EMAIL_ACCOUNT,
+      pass: EMAIL_PASSWORD
+    }
+  });
+
+  const info = await transporter.sendMail({
+    from: `"Easydeal Emailer" <${EMAIL_ACCOUNT}>`,
+    to: `${to}`,
+    subject: title,
+    text: content
+  });
+
+  return info;
+}
+
+exports.handler = async (req) => {
+  const { email, code } = JSON.parse(eq.body);
+  const title = `Your register verify code`;
+  const content = `Your register verify code is: ${code}`;
+
+  let body;
+  try {
+    const res = await sendTo(email, title, content);
+    body = {
+      success: true,
+      messageInfo: res.messageId
+    }
+  } catch(err) {
+    body = {
+      success: false,
+      message: err.toString()
+    }
+  }
+  return { 
+    statusCode: 200,
+    body: JSON.stringify(body)
+  }
+}
